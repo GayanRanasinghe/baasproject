15f7c99d5db688c62716fcee9730b9a5f8098c7c
gayanranasinghe <gayanmf@gmail.com>
2024-03-24 10:21:16 -0600

    New changes with Firebase

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,26 +1,30 @@
 // src/firebase/index.js
+
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { getDatabase } from "firebase/database"; // Import the getDatabase function
 import { LocalStorage } from 'quasar'
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDoPw1-wMAEP4mYYwJTfj3dIK5um14v5JQ",
-  authDomain: "bassproject1-421c8.firebaseapp.com",
-  projectId: "bassproject1-421c8",
-  storageBucket: "bassproject1-421c8.appspot.com",
-  messagingSenderId: "576538986740",
-  appId: "1:576538986740:web:d9abdde05c9f5f7b41b6a1",
-  measurementId: "G-GV1JBQEMYW"
+  apiKey: "AIzaSyAPtZZ5FLTO5NKUOcJKgr42WXnbdd2ntzM",
+  authDomain: "testproject1-b4c4f.firebaseapp.com",
+  projectId: "testproject1-b4c4f",
+  storageBucket: "testproject1-b4c4f.appspot.com",
+  messagingSenderId: "283783568329",
+  appId: "1:283783568329:web:02bb9834d4f14dd81ae618",
+  measurementId: "G-02PB0JQQK1"
 };
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage();
 export const rtdb = getDatabase(app); // Initialize the Realtime Database
+
 
 onAuthStateChanged(auth, (user) => {
  if (user) {
